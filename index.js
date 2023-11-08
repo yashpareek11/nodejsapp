4803980446b1ebe9e49b72858cc2860fb896f095
yashpareek11 <60838053+yashpareek11@users.noreply.github.com>
2023-11-08 12:59:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!.....  This is Node js Project....')
+  res.send('This is Node js Project....')
 })
 
 app.listen(port, () => {
